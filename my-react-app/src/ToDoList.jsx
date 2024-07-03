2cb1849df45918c82b7efb4b2f3584053dad9723
Callum Jones <108936019+Devilswrathed@users.noreply.github.com>
2024-07-03 09:05:39 +0000

    added a time feature

--- my-react-app/src/ToDoList.jsx
+++ my-react-app/src/ToDoList.jsx
@@ -1,11 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function ToDoList() {
 
     const [tasks, setTasks] = useState(
-        [        ]);
+        []);
 
     const [newTask, setNewTask] = useState("");
+
+    const [selectedTime, setSelectedTime] = useState("");
+
 
     function handleInputChange(event) {
         setNewTask(event.target.value);
@@ -13,15 +16,15 @@ function ToDoList() {
 
     function addTask() {
 
-        if(newTask.trim() !== ""){
+        if (newTask.trim() !== "") {
             setTasks(t => [...t, newTask]);
             setNewTask("");
-        
+
         }
 
     }
 
-    function deleteTask(index){
+    function deleteTask(index) {
 
         const updatedTasks = tasks.filter((_, i) => i !== index);
         setTasks(updatedTasks);
@@ -29,26 +32,32 @@ function ToDoList() {
     }
 
     function moveTaskUp(index) {
-        if(index > 0){
+        if (index > 0) {
             const updatedTasks = [...tasks];
-            [updatedTasks[index], updatedTasks[index - 1]] = 
-            [updatedTasks[index - 1], updatedTasks[index]];
+            [updatedTasks[index], updatedTasks[index - 1]] =
+                [updatedTasks[index - 1], updatedTasks[index]];
 
             setTasks(updatedTasks);
         }
     }
 
     function moveTaskDown(index) {
-        if(index < tasks.length - 1){
+        if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
-            [updatedTasks[index], updatedTasks[index + 1]] = 
-            [updatedTasks[index + 1], updatedTasks[index]];
+            [updatedTasks[index], updatedTasks[index + 1]] =
+                [updatedTasks[index + 1], updatedTasks[index]];
 
             setTasks(updatedTasks);
         }
     }
-    
-    return(
+
+    function handleTimeChange(event) {
+        setSelectedTime(event.target.value);
+    }
+
+
+
+    return (
         <div className="to-do-list">
 
             <h1>
@@ -56,55 +65,80 @@ function ToDoList() {
             </h1>
 
             <div className="">
-                <input 
-                type="text"
-                placeholder="Enter A task..."
-                value={newTask}
-                onChange={handleInputChange}
+                <input
+                    type="text"
+                    placeholder="Enter A task..."
+                    value={newTask}
+                    onChange={handleInputChange}
 
                 />
 
-                <button 
-                className="add-button"
-                onClick={addTask}
+                <select value={selectedTime} onChange={handleTimeChange}>
+
+                    <option value="">Select Time</option>
+                    <option value="09:00">09:00 AM</option>
+                    <option value="09:00">09:30 AM</option>
+                    <option value="09:00">10:00 AM</option>
+                    <option value="09:00">10:30 AM</option>
+                    <option value="09:00">11:00 AM</option>
+                    <option value="09:00">11:30 AM</option>
+                    <option value="09:00">12:00 PM</option>
+                    <option value="09:00">12:30 PM</option>
+                    <option value="09:00">13:00 PM</option>
+                    <option value="09:00">13:30 PM</option>
+                    <option value="09:00">14:00 PM</option>
+                    <option value="09:00">14:30 PM</option>
+                    <option value="09:00">15:00 PM</option>
+                    <option value="09:00">15:30 PM</option>
+                    <option value="09:00">16:00 PM</option>
+
+                </select>
+
+                <button
+                    className="add-button"
+                    onClick={addTask}
                 >
                     Add
                 </button>
+
+
             </div>
 
             <ol>
-                {tasks.map((task, index) => 
+                {tasks.map((task, index) =>
                     <li key={index}>
 
                         <span className="text">{task}</span>
-                        
+
+                        {selectedTime && <p>{selectedTime}</p>}
+
                         <button
                             className="delete-button"
                             onClick={() => deleteTask(index)}>
 
                             Delete
-                        
+
                         </button>
 
                         <button
                             className="move-button"
                             onClick={() => moveTaskUp(index)}>
-                                
+
                             👆
-                        
+
                         </button>
 
                         <button
                             className="move-button"
                             onClick={() => moveTaskDown(index)}>
-                                
+
                             👇
-                        
+
                         </button>
 
-                        
+
                     </li>
-            )}
+                )}
             </ol>
 
         </div>
